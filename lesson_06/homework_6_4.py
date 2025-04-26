77139ee6388279dc2e5bae96ecd0fe6411c456f9
vitalii-didkovskyi <didkovskyi.vitalii@gmail.com>
2025-04-27 00:23:27 +0300

    Виконана дз №5 (#5)

---
+++ lesson_06/homework_6_4.py
@@ -0,0 +1,16 @@
+'''Є ліст з числами, порахуйте сумму усіх ПАРНИХ чисел в цьому лісті'''
+
+lst1 = list(range(10))
+
+# n = int(input("Введіть число, з якого буде складатись список: "))
+# lst1 = [int(input(f"Введіть число {k+1}: ")) for k in range(n)]
+
+print(f"Ваш список чисел для підрахунку:\n{lst1}\n")
+records = []
+for k in lst1:
+    if k % 2 == 0: # парні числа в списку
+        records.append(k)
+        lst2 = sum(records)
+
+# lst2 = sum(k for k in lst1 if k % 2 == 0)
+print(f"Сума парних чисел з списку: {lst2}")
